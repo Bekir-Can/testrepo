c9300b8aa6d0f8958510d32e1d473d5443c69302
Bekir-Can <ruikosta03@gmail.com>
2021-02-15 16:02:41 +0200

    bekir change

--- src/test/java/testfile.java
+++ src/test/java/testfile.java
@@ -3,6 +3,8 @@ public class testfile {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello world");
+        System.out.println("This is going to be change by Bekir branch");
+        //
 
     }
 
